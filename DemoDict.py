c2b829d3d50eb0ca7b2b6c00db0d2975d253632e
papasmf1 <papasmf1@gmail.com>
2021-10-18 15:09:00 +0900

    first

---
+++ DemoDict.py
@@ -0,0 +1,19 @@
+# DemoDict.py 
+device = {"아이폰":5, "아이패드":10, "윈도우":20}
+print( device )
+print( len(device) )
+print( device["아이폰"] )
+device["맥프레"] = 15 
+device["아이폰"] = 6 
+print( device )
+del device["아이폰"]
+#중단점(Break Point):디버깅하는 모드에서는 멈춤
+for item in device.items():
+    print(item)
+
+print("---key, value---")
+for k,v in device.items():
+    print(k,v)
+
+
+
---
+++ test.py
@@ -0,0 +1,5 @@
+# test.py 
+print("vs code")
+print("qqqq")
+
+
