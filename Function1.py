554c6b8cad0f43181db34feaa23b15886a3eb91f
papasmf1 <papasmf1@gmail.com>
2021-10-18 16:32:55 +0900

    함수연습3

--- Function1.py
+++ Function1.py
@@ -14,3 +14,30 @@ def swap(x,y):
 #호출
 print( swap(5,6) )
 
+print("---불변형식과 가변형식---")
+a = 1.2 
+print( id(a) )
+a = 2.3 
+print( id(a) )
+lst = [1,2,3]
+print( id(lst) )
+lst.append(4)
+print( id(lst) )
+
+#기본값이 있는 경우
+def times(a=10, b=20):
+    return a*b 
+
+#호출
+print( times() )
+print( times(5) )
+print( times(5,6) )
+
+#키워드인자(파라메터명 명시)
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port 
+    return strURL 
+
+#호출
+print( connectURI("credu.com", "80") )
+print( connectURI(port="80", server="credu.com") )
