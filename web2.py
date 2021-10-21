9c49b335fb731078d03d5c4b0f0055f6404447af
papasmf1 <papasmf1@gmail.com>
2021-10-21 11:35:39 +0900

    네이버 웹툰 저장하기

--- web2.py
+++ web2.py
@@ -4,21 +4,25 @@ import urllib.request
 #검색 
 from bs4 import BeautifulSoup
 
-#웹사이트에서 검색한 결과를 문자열로 받기
-data = urllib.request.urlopen("http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
-#검색이 용이한 객체 생성
-soup = BeautifulSoup(data, "html.parser")
+#파일에 저장하기
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
+#수열을 만드는 함수
+for i in range(1,6):
+    #기본 주소
+    url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+    print("조립한주소:", url)
+    #웹사이트에서 검색한 결과를 문자열로 받기
+    data = urllib.request.urlopen(url)
+    #검색이 용이한 객체 생성
+    soup = BeautifulSoup(data, "html.parser")
 
-# <td class="title">
-#     <a href="/webtoon/detail?titleId=20853&no=50&weekday=fri">격렬한 나의 하루</a>
-# </td>
-#조건을 주고 필터링
-cartoons = soup.find_all("td", class_="title")
-#10개를 가져왔다면 
-print("갯수:{0}".format(len(cartoons)))
-#첫번째 방에 있는 아이템 슬라이싱 
-title = cartoons[0].find("a").text 
-link = cartoons[0].find("a")["href"]
-print( title )
-print( link )
+    cartoons = soup.find_all("td", class_="title")
+
+    for item in cartoons:
+        title = item.find("a").text
+        print( title.strip() )
+        f.write(title.strip() + "\n")
+
+f.close()
+print("웹 크롤링 종료~~")
 
